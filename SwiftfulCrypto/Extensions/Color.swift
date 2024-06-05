07b957495d6488fa4287fe8d00789119946dd147
Abi <amariano.fw@gmail.com>
2024-06-05 13:59:44 +0800

    Added color extension

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Abi M on 6/5/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
