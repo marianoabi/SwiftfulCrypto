d81284980fad11301ab48c26634182e7ff3440c6
Abi <amariano.fw@gmail.com>
2024-06-10 11:06:59 +0800

    Added a reusable networking layer

--- SwiftfulCrypto/Services/CoinDataService.swift
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -19,28 +19,11 @@ class CoinDataService {
     private func getCoins() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
 
-        coinsSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { output -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinsSubscription = NetworkingManager.download(url: url)
             .decode(type: [Coin].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("DEBUG: Failed to fetch coins with error: \(error.localizedDescription)")
-                }
-            } receiveValue: { [weak self] returnedCoins in
-                print("Did receive new coins value")
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
-//                self?.coinsSubscription?.cancel()
-            }
+                self?.coinsSubscription?.cancel()
+            })
     }
 }
